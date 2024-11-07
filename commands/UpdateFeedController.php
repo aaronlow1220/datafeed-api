e6a9097b49bf40115dfa37e8a3da49127a2d76d8
aaronlow1220 <117196390+aaronlow1220@users.noreply.github.com>
2024-11-07 15:27:23 +0800

    feat: Add update cli cmd for specific feed

--- commands/UpdateFeedController.php
+++ commands/UpdateFeedController.php
@@ -11,8 +11,18 @@ use app\components\platform\PlatformRepo;
 use yii\console\Controller;
 use yii\helpers\BaseConsole;
 
+/**
+ * This controller is used to update feed.
+ *
+ * @author Aaron Low <aaron.low@atelli.ai>
+ */
 class UpdateFeedController extends Controller
 {
+    /**
+     * @var string
+     */
+    public $feed;
+
     /**
      * Declared options.
      *
@@ -22,6 +32,7 @@ class UpdateFeedController extends Controller
     public function options($actionID): array
     {
         return array_merge(parent::options($actionID), [
+            'feed',
         ]);
     }
 
@@ -33,6 +44,7 @@ class UpdateFeedController extends Controller
     public function optionAliases()
     {
         return array_merge(parent::optionAliases(), [
+            'f' => 'feed',
         ]);
     }
 
@@ -91,6 +103,53 @@ class UpdateFeedController extends Controller
                 echo 'Feed file: '.$feedFile['id'].' has been updated'."\n";
             }
             echo $this->ansiFormat("Successfully update all feeds\n", BaseConsole::BG_GREEN);
+        } catch (Exception $e) {
+            echo 'Error: '.$e->getMessage()."\n";
+        }
+    }
+
+    /**
+     * Update feed by id, Only active feed is allowed.
+     *
+     * @param FeedFileRepo $feedFileRepo
+     * @param FileRepo $fileRepo
+     * @param PlatformRepo $platformRepo
+     * @param ClientRepo $clientRepo
+     * @param DatafeedService $datafeedService
+     * @return void
+     */
+    public function actionUpdateFeed(FeedFileRepo $feedFileRepo, FileRepo $fileRepo, PlatformRepo $platformRepo, ClientRepo $clientRepo, DatafeedService $datafeedService)
+    {
+        try {
+            $resultPath = __DIR__.'/../runtime/files/result';
+            if (!is_dir($resultPath)) {
+                mkdir($resultPath, 0777, true);
+            }
+            $feedFile = $feedFileRepo->findOne(['id' => $this->feed, 'status' => '1']);
+            $client = $clientRepo->find()->where(['id' => $feedFile['client_id']])->one();
+            $platform = $platformRepo->find()->where(['id' => $feedFile['client_id']])->one();
+
+            $filePath = sprintf('%s/%s_%s_%s_feed.csv', $resultPath, uniqid(), $client['name'], $platform['name']);
+
+            $datafeedService->export($platform, $client, $feedFile, $filePath);
+
+            $data = [
+                'mime' => 'text/csv',
+                'extension' => 'csv',
+                'filename' => basename($filePath),
+                'path' => $filePath,
+                'size' => filesize($filePath),
+            ];
+
+            $file = $fileRepo->create($data);
+
+            $feedFileParams = [
+                'file_id' => $file['id'],
+            ];
+
+            $feedFile = $feedFileRepo->update($feedFile, $feedFileParams);
+
+            echo $this->ansiFormat('Successfully update feed:'.$feedFile['id']."\n", BaseConsole::BG_GREEN);
         } catch (Exception $e) {
             echo 'Error: '.$e->getMessage()."\n";
         }
